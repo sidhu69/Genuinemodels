21d55a40e3c5cd568e3e5153089cb60e23d31123
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-12-15 16:37:05 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,28 @@
+MODELS = [
+    {
+        "id": 1,
+        "name": "Riya",
+        "age": 19,
+        "photos": [
+            "https://example.com/riya1.jpg",
+            "https://example.com/riya2.jpg"
+        ],
+        "services": {
+            "Video Call": 600,
+            "Chat": 300
+        },
+        "verified": True
+    },
+    {
+        "id": 2,
+        "name": "Anu",
+        "age": 21,
+        "photos": [
+            "https://example.com/anu1.jpg"
+        ],
+        "services": {
+            "Video Call": 800
+        },
+        "verified": True
+    }
+]
